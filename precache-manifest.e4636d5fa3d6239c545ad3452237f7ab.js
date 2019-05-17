3cd6027222e0e0ae7737125d6fcd2c8005056608
Zane Rankin <zwrankin@gmail.com>
2019-05-17 18:50:29 -0500

    Updates

---
+++ precache-manifest.e4636d5fa3d6239c545ad3452237f7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d51c82fbfeb5fc8d0f2bfc3acf83adb",
+    "url": "/frankie-is-hungry/index.html"
+  },
+  {
+    "revision": "e08343d410325baca886",
+    "url": "/frankie-is-hungry/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/frankie-is-hungry/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e08343d410325baca886",
+    "url": "/frankie-is-hungry/static/js/main.47e4c2b3.chunk.js"
+  },
+  {
+    "revision": "7e037d06e20fb73a9053",
+    "url": "/frankie-is-hungry/static/js/runtime~main.fe4bd197.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frankie-is-hungry/static/media/logo.5d5d9eef.svg"
+  }
+]);
