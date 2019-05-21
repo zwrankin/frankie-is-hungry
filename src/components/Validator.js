9f16ea0d8dda85cb54c01a515b3980409577a055
Zane Rankin <zwrankin@gmail.com>
2019-05-20 20:07:36 -0500

    add conditional rendering of text

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
-const Validator = props => (
-    // let Valid = false;
-    // const Valid = props.query.toLowerCase() === "fish"
-    <div className="frankie"
-         style={{color: props.query.toLowerCase() === "fish" ? "green" : "red"}}>
-        { props.query.toLowerCase() === "fish" ? "Yummmm" : "I WANT FISH"}
-    </div>
-);
+function EmptyResponse(props) {
+    const text = "FEED ME \n RIGHT MEOW!"
+    return <p className="frankie" style={{color: "red" }}> {text} </p>
+}
+
+function FishResponse(props) {
+    return <div className="frankie" style={{color: "green" }}> Yummmmm! </div>
+}
+
+function NotFishResponse(props) {
+    const text = `I WANT FISH \n NOT ${props.query.toUpperCase()}`
+    return (
+        <div>
+            <img className="img__query"
+                src={`http://loremflickr.com/600/400/${props.query.toLowerCase()}`}
+                alt={`${props.query.toLowerCase()}`}/>
+            <div className="frankie">{text}</div>
+        </div>
+    )
+}
+
+function Validator(props) {
+    if (props.query.toLowerCase() === "fish") {
+        return ( < FishResponse / > )
+    }
+    else if (props.query === "") {
+        return ( < EmptyResponse / > )
+    }
+    else {
+        return ( < NotFishResponse query = {props.query}/ > )
+    }
+}
+
 
 export default Validator;
